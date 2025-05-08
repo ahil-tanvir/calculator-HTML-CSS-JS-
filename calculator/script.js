a7f1c9d4cbe5ced7d197ab9eebc366c2bc2d596e
Ahil Tanvir <ahiltanvirbarbhuiya13@outlook.com>
2025-05-08 20:28:14 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,26 @@
+let string = "";
+const input = document.querySelector('.display input');
+const buttons = document.querySelectorAll('.button');
+
+ buttons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    const value = e.target.innerText;
+
+    if (value === 'C') {
+      string = "";
+      input.value = "";
+    } else if (value === '=') {
+      try {
+        string = eval(string);
+        input.value = string;
+      } catch {
+        input.value = "Error";
+        string = "";
+      }
+    } else {
+      string += value;
+      input.value = string;
+    }
+  });
+}); 
+
